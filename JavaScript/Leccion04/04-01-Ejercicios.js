ba6d2e8f9b48a5c13a815952363b4b7687797144
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-06-17 18:42:27 -0300

    Actualización de archivos de javascript, añado ejercicios

--- JavaScript/Leccion04/04-01-Ejercicios.js
+++ JavaScript/Leccion04/04-01-Ejercicios.js
@@ -1,54 +1,81 @@
-// AMPLIANDO EL USO DE VAR, LET Y CONST
-/*
-    Con var se pueden reasignar los valores en cualquier momento, este forma parte del ámbito globarl.
-    Un error es que se sobreescriba 
+// Ejercicio 1: Calcular estación del año
+//  Ingresar un mes del año, el valor debe ser entre 1 y 12, 
+//  luego hay que indicar en qué estación del año se está:
+//     VERANO             OTOÑO           INVIERNO        PRIMAVERA
+//  21/12 al 21/03    21/03 al 21/06  21/06 al 21/09    21/09 al 21/12
+//     1, 2, 3            4, 5, 6          7, 8, 9         10, 11, 12
+
+//IF - IF ELSE
+let mes = 5;
+let estacion;
+
+if (mes == 1 || mes == 2 || mes == 3){
+    estacion = "VERANO";
+}
+else if (mes == 4 || mes == 5 || mes == 6){
+    estacion = "OTOÑO";
+}
+else if (mes == 7 || mes == 8 || mes == 9){
+    estacion = "INVIERNO";
+}
+else if (mes == 10 || mes == 11 || mes == 12){
+    estacion = "PRIMAVERA";
+}
+else{
+    estacion = "Número de mes inexistente";
+}
+
+console.log(estacion);
+
+//SWITCH (Igual a la de Java)
+switch(mes){
+    case 1: case 2: case 3:
+        estacion = "VERANO";
+    break;
+
+    case 4: case 5: case 6:
+        estacion = "OTOÑO";
+    break;
+
+    case 7: case 8: case 9:
+        estacion = "INVIERNO";
+    break;
+
+    case 10: case 11: case 12:
+        estacion = "PRIMAVERA";
+    break;
+    
+    default:
+        estacion = "Número de mes inexistente";  
+}
+
+console.log(estacion);
+
+//Ejercicio 2: Hora del día
+/* 
+    De 6 a 11 -> 'Good Morning!'
+    De 12 a 16 -> 'Good Afternoon!'
+    De 17 a 19 -> 'Good Evening!'
+    De 20 a 23 -> 'Good Night!'
 */
 
-var nombre = "Ana";
-var nombre = "Paula";
-console.log(nombre);
+let hora = 5;
+let mensaje;
 
-function saludar(){
-    var nombre2 = "Lucas";
-    console.log(nombre2);
+if (hora >= 6 || hora <= 11){
+    mensaje = "Good Morning!";
+}
+else if (hora == 12 || hora <= 16){
+    mensaje = "Good Afternoon!";
+}
+else if (hora == 17 || hora <= 19){
+    mensaje = "Good Evening!";
+}
+else if (hora == 20 || hora <= 23){
+    mensaje = "Good Night!";
+}
+else{
+    mensaje = "Hora no identificada";
 }
 
-console.log(nombre2); //No lee el dato de la función
-
-if(true){
-    var edad = 34;
-    console.log(edad);
-}
-
-console.log(edad); //En la función, funcionó perfectamente, mientras que en la estructura if, falló.
-
-/*
-    let: puede ser reasignada en cualquier momento.
-    La diferencia es que su ámbito es de bloque, solo disponible dentro de un bloque de llaves o dentro de una función.
-*/
-
-function saludar2(){
-    var nombre3 = "Diego";
-    console.log(nombre3);
-}
-
-//console.log(nombre3);
-
-if(true){
-    let edad2 = 33;
-    console.log(edad2);
-}
-
-//console.log(edad2);
-
-/*
-    const: se utiliza para valores constantes que no pueden ser resignadas.
-*/
-
-const fechaNacimiento = 2005;
-console.log(fechaNacimiento);
-
-fechaNacimiento = 2010;
-console.log(fechaNacimiento); // Solo se ejecuta la consola anterior.
-
-
+console.log(mensaje);
