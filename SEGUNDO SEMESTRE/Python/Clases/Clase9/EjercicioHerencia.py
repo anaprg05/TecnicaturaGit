8b31cc3db0ebd3576a64281958d8d9ed03b18fac
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-10-23 12:01:08 -0300

    Agrego PRIMER SEMESTRE y SEGUNDO SEMESTRE al repositorio

---
+++ SEGUNDO SEMESTRE/Python/Clases/Clase9/EjercicioHerencia.py
@@ -0,0 +1,19 @@
+# Ejercicio: Uso de herencia
+'''
+    Definir una clase padre llamada 'Vehiculo' y dos clases hijas llamadas 'Auto' y 'Bicicleta', las cuales
+    heredan de la clase padre Vehiculo. La clase padre debe tener los siguientes atributos y métodos:
+        Vehiculo(Clase padre):
+        - Atributos (color, ruedas)
+        - Métodos (__init__(color, ruedas) y __str__())
+
+        Auto(Clase hija de Vehiculo):
+        - Atributos (velocidad(km/hr))
+        - Métodos (__init__(color, ruedas, velocidad) y __str__())
+        
+        Bicicleta(Clase hija de Vehiculo):
+        - Atributos (tipo(urbana/montaña/etc))
+        - Métodos (__init__(color, ruedas, tipo) y __str__())
+
+    Crear un objeto de cada clase
+'''
+
