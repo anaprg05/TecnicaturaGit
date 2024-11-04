1fb720a5d81d7ffc8a84cd86abeba66180710026
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-11-04 00:37:13 -0300

    Actualizo carpeta de JavaScript

--- SEGUNDO SEMESTRE/JavaScript/Leccion05/05-02-Clases.js
+++ SEGUNDO SEMESTRE/JavaScript/Leccion05/05-02-Clases.js
@@ -7,6 +7,15 @@
 // Clase padre
 // Definición de clase -> utilizar palabra reservada 'class'
 class Persona{
+    // Atributos estáticos
+    // Definición de atributo perteneciente a la clase y no
+    // a un objeto
+    static contadorPersonas = 0;
+
+    // Atributo No Estático
+    // Se asocia a los objetos
+    // email = 'Valor default email';
+
     // Definicón de constructor
     // Si no se define un constructor, JavaScript agrega 
     // uno vacío de forma automática
@@ -14,6 +23,11 @@ class Persona{
         // Inicialización de atributos
         this._nombre = nombre;
         this._apellido = apellido;
+
+        // Cada vez que se instancie una nueva persona, el contador
+        // incrementará y recibirá un ID único
+        this.idPersona = ++ Persona.contadorPersonas;
+        // console.log('Se incrementa el contador: ' + Persona.contadorObjetosPersona);
     }
 
     // MÉTODOS SET Y GET
@@ -46,7 +60,7 @@ class Persona{
 
     // Definición de método
     nombreCompleto(){
-        return this._nombre + ' ' + this._apellido;
+        return this.idPersona + '. ' +this._nombre + ' ' + this._apellido;
     }
 
     // Método To String
@@ -58,6 +72,17 @@ class Persona{
         // Puede llamar un método tanto de la clase padre
         //  como de la clase hija (depende de la referencia)
         return this.nombreCompleto();
+    }
+
+    // Método Static
+    // La salida solo se verá en la consola
+    static saludar(){
+        console.log('Saludos desde este método static.');
+    }
+
+    // El método estático puede pasar como argumentos un objeto
+    static saludar2(persona){
+        console.log(persona.nombre + ' ' + persona.apellido);
     }
 }
 
@@ -123,3 +148,35 @@ console.log(empleado1.nombreCompleto());
 console.log(empleado1.toString());
 // Método clase padre
 console.log(persona1.toString());
+
+// Un método estático se asocia a una clase pero no con los objetos
+// Es decir, no se utiliza desde el objeto
+// persona1.saludar();
+
+// Los métodos estáticos se utilizan desde la clase
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+// Al ser un método estático, se tiene que llamar a través de
+// la clase, no del objeto
+// console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+// Los elementos estáticos no pueden acceder desde la clase, 
+// pueden acceder desde los objetos de clase y los objetos 
+// heredados
+// console.log(persona1.email);
+// console.log(empleado1.email);
+// console.log(Persona.email);
+
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+
+let persona3 = new Persona('Sebastián', 'Rodríguez');
+console.log(persona3.toString());
+console.log(Persona.contadorPersonas);
