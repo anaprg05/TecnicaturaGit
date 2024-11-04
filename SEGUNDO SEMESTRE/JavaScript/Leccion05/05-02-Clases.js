0993e89df4fdfdd15c6e7f4267266b9a67dc9a0f
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-11-03 21:23:46 -0300

    Actualizo carpeta de JavaScript

--- SEGUNDO SEMESTRE/JavaScript/Leccion05/05-02-Clases.js
+++ SEGUNDO SEMESTRE/JavaScript/Leccion05/05-02-Clases.js
@@ -1,3 +1,10 @@
+// Hosting antes de crear la clase
+// No se puede crear una variable, ya que no se puede 
+// acceder a una clase sin haberla inicializado o definido 
+// con anterioridad
+// let persona3 = new Persona('Nelson', 'Ríos');
+
+// Clase padre
 // Definición de clase -> utilizar palabra reservada 'class'
 class Persona{
     // Definicón de constructor
@@ -5,13 +12,81 @@ class Persona{
     // uno vacío de forma automática
     constructor(nombre, apellido){
         // Inicialización de atributos
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    // MÉTODOS SET Y GET
+    // Las propiedades no pueden tener el mismo nombre que 
+    // los atributos
+
+    // Método Get
+    // Leer la información del atributo
+    get nombre(){
+        return this._nombre;
+    }
+
+    // Método Set
+    // Modificar valor del atributo
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    // TAREA
+    // Crear el método get y set para el atributo de apellido,
+    // luego modificar el apellido a través del  set y mostrar
+    // con un console.log lo que es el get donde muestra el 
+    // resultado obtenido.
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+// Clase hija -> extiende de la clase padre
+class Empleado extends Persona{
+    // Definición de constructor
+    constructor(nombre, apellido, departamento){
+        // La primer línea de la clase hija se utiliza para 
+        // llamar al constructor de la clase padre
+        // super se accede a través del this y se utiliza 
+        // para llamar al constructor de la clase padre
+        super(nombre, apellido);
+        // Inicialización de atributos
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento;
     }
 }
 
 // Creación de objetos
-let persona1 = new Persona('Ana', 'Ríos');
-console.log(persona1);
-let persona2 = new Persona('Lucas', 'Cerbino');
-console.log(persona2);
+let persona1 = new Persona('Ana', 'Garín');
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+
+persona1.nombre = 'Paula';
+persona1.apellido = 'Ríos';
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+// console.log(persona1)
+
+let persona2 = new Persona('Lautaro', 'Ríos');
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+
+persona2.nombre = 'Lucas';
+persona2.apellido = 'Cerbino';
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+// console.log(persona2)
+
+let empleado1 = new Empleado('Nelson', 'Ríos', 'Matricería');
+console.log(empleado1);
--- SEGUNDO SEMESTRE/JavaScript/Leccion05/TeoriaClases.md
+++ SEGUNDO SEMESTRE/JavaScript/Leccion05/TeoriaClases.md
@@ -1,18 +1,26 @@
 CLASES
 
-A diferencia de los 'Objetos' una clase es una plantilla, en la cual se 
-definirán los atributos y métodos que tendrán los objetos. Además, en las 
-clases se pueden crear instancias. Es decir, los objetos son instancias de 
-una clase, un objeto puede tener valores en sus atributos.
+    A diferencia de los 'Objetos' una clase es una plantilla, en la cual se 
+definirán los atributos y métodos que tendrán los objetos. Además, en las clases
+se pueden crear instancias. Es decir, los objetos son instancias de una clase, 
+un objeto puede tener valores en sus atributos.
 
-A la hora de crear una clase, realmente no contiene valores y, si los tiene,
-se aplicarán a todos los objetos que se creen. Pero, la idea de una clase es
-que sea una plantilla en la cual se puedan crear instancias/objetos.
+    A la hora de crear una clase, realmente no contiene valores y, si los tiene,
+se aplicarán a todos los objetos que se creen. Pero, la idea de una clase es que
+sea una plantilla en la cual se puedan crear instancias/objetos.
 
-Una clase va a contar con un nombre, con atributos y con métodos, los cuales
-el programador deberá designarle. Sin embargo, cada obejto debe contar con
-sus valores particulares, entonces los objetos no comparten información entre 
-ellos , cada uno tiene su propia información, pero hay excepciones a esta 
-regla.
-Si en la clase está indicado el atributo 'Nombre' todos los objetos contarán 
+    Una clase va a contar con un nombre, con atributos y con métodos, los cuales
+el programador deberá designarle. Sin embargo, cada obejto debe contar con sus 
+valores particulares, entonces los objetos no comparten información entre 
+ellos , cada uno tiene su propia información, pero hay excepciones a esta regla.
+    Si en la clase está indicado el atributo 'Nombre' todos los objetos contarán 
 con dicho atributo, pero cada objeto puede tener un nombre propio.
+
+-------------------------------------------------------------------------------
+HOSTING
+    - Levantar/ Izar
+
+    Cuando se está trabajando con clases, no se aplica el hosting. Es decir, 
+primero hay que definir la clase para poder utilizarla. En cambio, si se 
+trabaja con funciones, se puede llamar a la función y luego definirla ya que
+se aplica el concepto de hosting.
