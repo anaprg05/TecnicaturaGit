02507b60c7d081f1a4cca374d823fb27ffe6a715
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-11-07 13:32:08 -0300

    Actualizo carpeta Java

--- SEGUNDO SEMESTRE/JAVA/Leccion04/Herencia/src/test/TestHerencia.java
+++ SEGUNDO SEMESTRE/JAVA/Leccion04/Herencia/src/test/TestHerencia.java
@@ -1,10 +1,18 @@
 
 package test;
+
 import domain.Empleado;
+import domain.Cliente;
+
+import java.util.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Ana", 57000.0);
         System.out.println("empleado1 = " + empleado1);
+    
+        Cliente cliente1 = new Cliente(new Date(), true, "Lucas", 'M', 20, "Poeta Risso 429");
+        System.out.println("cliente1 = " + cliente1);
+        
     }
 }
