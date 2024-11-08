ed277b7f8e44cc82731ed722d59fe14d442e3f05
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-11-08 12:27:40 -0300

    Actualizo carpeta Java

--- SEGUNDO SEMESTRE/JAVA/Clase12/Ventas/src/ar/com/codesystem/ventas/test/VentasTest.java
+++ SEGUNDO SEMESTRE/JAVA/Clase12/Ventas/src/ar/com/codesystem/ventas/test/VentasTest.java
@@ -18,5 +18,41 @@ public class VentasTest {
         orden1.agregarProducto(producto2);
         
         orden1.mostrarOrden();
+        
+        /*
+        TAREA:
+            1. Crear más objetos de tipo Producto = 10
+            2. Crear más objetos de tipo Orden = 2
+        */
+        Producto producto3 = new Producto("Zapatillas", 45000.00);
+        Producto producto4 = new Producto("Cinturón", 2500.00);
+        Producto producto5 = new Producto("Medias", 500.00);
+
+        orden1.agregarProducto(producto3);
+        orden1.agregarProducto(producto4);
+        orden1.agregarProducto(producto5);
+        
+        orden1.mostrarOrden();
+        
+        Orden orden2 = new Orden();
+        
+        Producto producto6 = new Producto("Vestido", 25500.00);
+        Producto producto7 = new Producto("Pollera", 15000.00);
+        Producto producto8 = new Producto("Bufanda", 9000.00);
+        Producto producto9 = new Producto("Corbata", 6000.00);
+        Producto producto10 = new Producto("Guantes", 3000.00);
+        
+        orden2.agregarProducto(producto6);
+        orden2.agregarProducto(producto7);
+        orden2.agregarProducto(producto8);
+        orden2.agregarProducto(producto9);
+        orden2.agregarProducto(producto10);
+        
+        orden2.mostrarOrden();
+
+        Orden orden3 = new Orden();
+        Producto producto11 = new Producto("Collar", 30000.00);
+        orden3.agregarProducto(producto11);
+        orden3.mostrarOrden();
     }
 }
