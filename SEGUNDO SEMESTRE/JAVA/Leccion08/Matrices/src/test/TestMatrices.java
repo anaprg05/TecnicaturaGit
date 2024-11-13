6130470a4749f9e7887b7ebf99517a0e9daf2698
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2024-11-13 12:48:58 -0300

    Actualizo la carpeta Java

--- SEGUNDO SEMESTRE/JAVA/Leccion08/Matrices/src/test/TestMatrices.java
+++ SEGUNDO SEMESTRE/JAVA/Leccion08/Matrices/src/test/TestMatrices.java
@@ -10,5 +10,28 @@ public class TestMatrices {
         int edades[][] = new int[3][2];
         System.out.println("edades = " + edades);
         
+        // Llenado manual
+        edades[0][0] = 5;
+        // Misma fila, columna distinta
+        edades[0][1] = 7;
+        edades[1][0] = 8;
+        edades[1][1] = 4;
+        
+        System.out.println("edades[0][0] = " + edades[0][0]);
+        System.out.println("edades[0][1] = " + edades[0][1]);
+        System.out.println("edades[1][0] = " + edades[1][0]);
+        System.out.println("edades[1][1] = " + edades[1][1]);
+        
+        /*
+        TAREA:
+            Agregar valores para los dos índices faltantes e imprimirlos
+        */
+        
+        edades[2][0] = 1;
+        edades[2][1] = 10;
+        
+        System.out.println("edades[2][0] = " + edades[2][0]);
+        System.out.println("edades[2][1] = " + edades[2][1]);
+        
     }
 }
