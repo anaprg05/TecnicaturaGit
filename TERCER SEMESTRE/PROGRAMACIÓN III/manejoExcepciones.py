2f8b7bdb85747b2ded49c052489b6b51f2c37c7a
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2025-04-07 23:57:18 -0300

    Actualizo clase 1 de python

--- TERCER SEMESTRE/PROGRAMACIÓN III/manejoExcepciones.py
+++ TERCER SEMESTRE/PROGRAMACIÓN III/manejoExcepciones.py
@@ -1,1 +1,9 @@
 # Un error o excepción es cuando el programa acaba de manera abrupta.
+try:
+    10/0
+
+# La clase Exception es una clase padre de la clase Zero Divition Error.
+# Esta clase permite mandar a imprimir el error que ocurre.
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+
