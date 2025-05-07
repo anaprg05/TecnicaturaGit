d07d93d69d8631ebd5125b460f3f93fabb991b28
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2025-05-07 19:05:35 -0300

    Termino la clase 05 de python

---
+++ TERCER SEMESTRE/PROGRAMACIÓN III/Python/Clase05/BD/actualizar_registros.py
@@ -0,0 +1,36 @@
+# Importación de Base de Datos
+import psycopg2
+
+# Variable de conexión
+conexion = psycopg2.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='Test_BD',
+    client_encoding='utf8'
+)
+
+# print(conexion)
+
+# Bloque try 
+try:
+    # Bloque With
+    with conexion:
+        with conexion.cursor() as cursor:
+            # Actualizar o modificar un registro
+            # Sentencia UPDATE
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            # Tupla de valores
+            valores = ('Illa', 'Garín', 'igarin@gmail.com', 5)
+            # Función .execute
+            cursor.execute(sentencia, valores)
+            # Función .rowcount
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+# Excepción
+except Exception as e:
+    print(f'Ocurrió un error: {e}.')
+# Bloque finally
+finally: 
+    conexion.close()
