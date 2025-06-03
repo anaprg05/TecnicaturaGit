1a5538efae7fd72a02d469f80cf29c8dc6d44eeb
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2025-06-02 22:04:39 -0300

    Actualizo Clase09 de Python

---
+++ TERCER SEMESTRE/PROGRAMACIÓN III/Python/Clase09/capa_datos_persona/persona_dao.py
@@ -0,0 +1,22 @@
+from conexion import Conexion
+
+class PersonaDAO:
+    """
+    DAO -> Data Access Object
+    CRUD -> Create (Insertar)
+         -> Read (Seleccionar)
+         -> Update (Actualizar)
+    """
+
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s, WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
+
+    # Definición de los métodos de clase
+    @classmethod
+    def seleccionar(cls):
+        # Conexiones automáticas
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCurso() as cursor:
+                cursor.execute()
