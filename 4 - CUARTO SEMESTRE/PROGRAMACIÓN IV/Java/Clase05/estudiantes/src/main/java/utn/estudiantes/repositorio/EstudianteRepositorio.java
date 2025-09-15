d6daabcded62991918283475d5e65536b838f546
Ana Paula Ríos Garín <anapaulariosgarin@gmail.com>
2025-09-15 18:24:26 -0300

    Corrijo errores en Clase05 de Java

--- 4 - CUARTO SEMESTRE/PROGRAMACIÓN IV/Java/Clase05/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++ 4 - CUARTO SEMESTRE/PROGRAMACIÓN IV/Java/Clase05/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -1,7 +1,7 @@
 package utn.estudiantes.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2024;
 
 /*
 Antes se utilizaba la siguiente anotación:
@@ -12,6 +12,6 @@ Antes se utilizaba la siguiente anotación:
 Pero, gracias al uso de 'extends JpaRepository', ya no es necesario
 */
 
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
+public interface EstudianteRepositorio extends JpaRepository<Estudiantes2024, Integer> {
 
 }
